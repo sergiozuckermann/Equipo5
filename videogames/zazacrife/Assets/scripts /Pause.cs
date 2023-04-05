639ae0d6d3116cfe2ac3b63c19cf49c84ad21af8
Domingo Mora <A01783317.tec.mx>
2023-04-05 16:38:34 -0500

    Pause and Quit Game Script

---
+++ videogames/zazacrife/Assets/scripts /Pause.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu...");
+    }
+    public void QuitGame(){
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
