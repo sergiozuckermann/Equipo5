33da6dbd14d75571289592ab58397e31f7196709
Rafael Blanga <A01781442@tec.mx>
2023-04-11 12:05:42 -0600

    enemy return to position

--- videogames/zazacrife/Assets/Scripts/chasee.cs
+++ videogames/zazacrife/Assets/Scripts/chasee.cs
@@ -7,43 +7,62 @@ public class chasee : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] float speed;
     [SerializeField] float radius;
-     
-    private float distance;
+    [SerializeField] float returnPositionX; // Serialized field for x position of return position
+    [SerializeField] float returnPositionY; // Serialized field for y position of return position
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Get the size of the collider and set the radius to half its size
-        
-    }
+    private float distance;
 
     // Update is called once per frame
     void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
-        if (checkRatiusFromPlayer()){
-            chasePlayer();
+        if (CheckRadiusFromPlayer())
+        {
+            ChasePlayer();
         }
-        Debug.Log("Distance: " + distance);
-        
+        else
+        {
+            ReturnToPosition();
+        }
     }
-    bool checkRatiusFromPlayer(){
-        if(distance < radius){
-            // print in console the distance between the player and the enemy
-            
+
+    bool CheckRadiusFromPlayer()
+    {
+        if (distance < radius)
+        {
             return true;
         }
-        return false;
+        else
+        {
+            return false;
+        }
     }
-    void chasePlayer(){
-        
+
+    void ChasePlayer()
+    {
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle );  
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
+    void ReturnToPosition()
+    {
+        Vector2 returnPosition = new Vector2(returnPositionX, returnPositionY);
+        Vector2 direction = returnPosition - (Vector2)transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.position = Vector2.MoveTowards(this.transform.position, returnPosition, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
+    // Method to set the return position from console or another script
+    public void SetReturnPosition(float x, float y)
+    {
+        returnPositionX = x;
+        returnPositionY = y;
     }
 }
-
-   
