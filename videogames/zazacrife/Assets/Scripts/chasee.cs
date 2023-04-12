86b2566f045210b813c1a30fa3184eeca6980837
sergiozuckermann <a01024831@itesm.mx>
2023-04-11 19:52:26 -0600

    implemented bull animations

--- videogames/zazacrife/Assets/Scripts/chasee.cs
+++ videogames/zazacrife/Assets/Scripts/chasee.cs
@@ -11,6 +11,7 @@ public class chasee : MonoBehaviour
     [SerializeField] float returnPositionY; // Serialized field for y position of return position
 
     private float distance;
+     [SerializeField]  Animator animator;
 
     // Update is called once per frame
     void Update()
@@ -42,10 +43,22 @@ public class chasee : MonoBehaviour
     {
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        
+        animator.SetFloat("VelX", direction.x);
+        animator.SetFloat("VelY", direction.y);
+
+        if(direction.x == 0 && direction.y == 0)
+        {
+            animator.SetInteger("Walk", 0);
+        }
+        else
+        {
+            animator.SetInteger("Walk", 1);
+        }
     }
 
     void ReturnToPosition()
@@ -53,10 +66,23 @@ public class chasee : MonoBehaviour
         Vector2 returnPosition = new Vector2(returnPositionX, returnPositionY);
         Vector2 direction = returnPosition - (Vector2)transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        
+
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this.transform.position, returnPosition, speed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        animator.SetFloat("VelX", direction.x);
+        animator.SetFloat("VelY", direction.y);
+        if(direction.x == 0 && direction.y == 0)
+        {
+            animator.SetInteger("Walk", 0);
+        }
+        else
+        {
+            animator.SetInteger("Walk", 1);
+        }
     }
 
     // Method to set the return position from console or another script
@@ -65,4 +91,10 @@ public class chasee : MonoBehaviour
         returnPositionX = x;
         returnPositionY = y;
     }
+
+    
+
+       
+        
+
 }
