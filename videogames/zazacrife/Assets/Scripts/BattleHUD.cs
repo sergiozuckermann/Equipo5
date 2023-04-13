6a1b86297346863bfe795de8ec478c1c7034b543
sergiozuckermann <a01024831@itesm.mx>
2023-04-13 09:51:20 -0600

    Battle system is now functional

--- videogames/zazacrife/Assets/Scripts/BattleHUD.cs
+++ videogames/zazacrife/Assets/Scripts/BattleHUD.cs
@@ -2,24 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BattleHUD : MonoBehaviour
 {
 
-	public Text nameText;
-	public Slider hpSlider;
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI HPText;
+	//public Slider hpSlider;
 
 	public void SetHUD(unit unit)
 	{
+		HPText.text = unit.currentHP.ToString();
 		nameText.text = unit.unitName;
-		hpSlider.maxValue = unit.maxHP;
-		hpSlider.value = unit.currentHP;
+		
+		// hpSlider.maxValue = unit.maxHP;
+		// hpSlider.value = unit.currentHP;
 	}
 
 	public void SetHP(int hp)
-	{
-		hpSlider.value = hp;
-	}
+	 {
+	 	HPText.text = hp.ToString();
+	 }
 
 }
 
