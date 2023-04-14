2ae5424d8f6fd5f7da7e5d8f9cd53e50a935ce9c
sergiozuckermann <a01024831@itesm.mx>
2023-04-13 22:10:12 -0600

    Added enemy animations

--- videogames/zazacrife/Assets/Scripts/BattleSystem.cs
+++ videogames/zazacrife/Assets/Scripts/BattleSystem.cs
@@ -9,11 +9,18 @@ public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 public class BattleSystem : MonoBehaviour
 {
 
+public float time;
+private float etime;
+public float update;
+
 public GameObject playerPrefab;
 public GameObject enemyPrefab;
-
+private Animator animatore;
+private Animator animators;
 public Transform playerBattleStation;
 public Transform enemyBattleStation;
+public Transform playerattack;
+public Transform enemyattack;
 
 unit playerUnit;
 unit enemyUnit;
@@ -30,6 +37,7 @@ public BattleState state;
 // Start is called before the first frame update
 void Start()
 {
+    
     state = BattleState.START;
     StartCoroutine(SetupBattle());
 }
@@ -42,6 +50,10 @@ IEnumerator SetupBattle()
     GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
     enemyUnit = enemyGO.GetComponent<unit>();
 
+    animators = playerGO.GetComponentInChildren<Animator>();
+    animatore = enemyGO.GetComponentInChildren<Animator>();
+
+  
     dialogueText.text = "A minion of the ZAZA  " + enemyUnit.unitName + " approaches...";
 
     enemyHUD.SetHUD(enemyUnit);
@@ -66,10 +78,36 @@ IEnumerator SetupBattle()
         else
         {
             state = BattleState.ENEMYTURN;
-            enemyHUD.SetHP(enemyUnit.currentHP);
-            dialogueText.text = "You deal " + playerUnit.damage + " damage...";
 
+            
+
+            animators.SetInteger("State", 1);
+            while(etime < time){
+                yield return new WaitForSeconds(update);
+                float interpolationRatio = etime / time;
+                playerUnit.transform.position=  Vector3.Lerp(playerBattleStation.position, playerattack.position, interpolationRatio);
+                etime += update;
+            }
+
+            animators.SetInteger("State", 2);
             yield return new WaitForSeconds(2f);
+            dialogueText.text = "You deal " + playerUnit.damage + " damage...";
+            enemyHUD.SetHP(enemyUnit.currentHP);
+            animators.SetInteger("State", 3);
+            while(etime > 0){
+                yield return new WaitForSeconds(update);
+                float interpolationRatio = etime / time;
+
+                playerUnit.transform.position=  Vector3.Lerp(playerBattleStation.position, playerattack.position, interpolationRatio);
+                etime -= update;
+                
+            }
+
+            animators.SetInteger("State", 0);
+            
+            yield return new WaitForSeconds(1f);
+            
+            
             StartCoroutine(EnemyTurn());
         }
   
@@ -77,10 +115,38 @@ IEnumerator SetupBattle()
 
  IEnumerator EnemyTurn()
  {
-     
-     dialogueText.text = enemyUnit.unitName + " attacks!";
+     animatore.SetInteger("State", 1);
+     while(etime < time){
+                yield return new WaitForSeconds(update);
+                float interpolationRatio = etime / time;
 
+                enemyUnit.transform.position=  Vector3.Lerp(enemyBattleStation.position, enemyattack.position, interpolationRatio);
+                etime += update;
+                
+            }
+     
+     
+     animatore.SetInteger("State", 2);
+     dialogueText.text = enemyUnit.unitName + " attacks!";
+     yield return new WaitForSeconds(2f);
+     
+     
+     animatore.SetInteger("State", 3);
+     
+     while(etime > 0){
+                yield return new WaitForSeconds(update);
+                float interpolationRatio = etime / time;
+
+                enemyUnit.transform.position=  Vector3.Lerp(enemyBattleStation.position, enemyattack.position, interpolationRatio);
+                etime -= update;
+                
+            }
+     
+     
+     animatore.SetInteger("State", 0);
      yield return new WaitForSeconds(1f);
+
+     
 
      bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
 
@@ -104,9 +170,11 @@ IEnumerator SetupBattle()
  {
      if(state == BattleState.WON)
      {
+        animatore.SetInteger("State", 4);
          dialogueText.text = "You won the battle!";
      } else if (state == BattleState.LOST)
      {
+        animators.SetInteger("State", 5);
          dialogueText.text = "You were defeated.";
      }
  }
@@ -121,9 +189,14 @@ IEnumerator SetupBattle()
         playerUnit.Heal(5);
 
         state = BattleState.ENEMYTURN;
-
+  
+        animators.SetInteger("State", 4);
+        yield return new WaitForSeconds(1f);
         playerHUD.SetHP(playerUnit.currentHP);
         dialogueText.text = "You healed for 5.";
+        yield return new WaitForSeconds(1f);
+        animators.SetInteger("State", 0);
+        
 
         yield return new WaitForSeconds(2f);
 
