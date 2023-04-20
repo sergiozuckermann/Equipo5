7b9e0637985454fb4c3b0e025393e0532934a246
Domingo Mora <A01783317.tec.mx>
2023-04-20 14:46:38 -0600

    Login

---
+++ videogames/zazacrife/Assets/Scripts/InputBoxLogger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputBoxLogger : MonoBehaviour
+{
+    public TMP_InputField username;
+    public TMP_InputField password;
+
+    public void LogInput()
+    {
+        string usernameM = username.text;
+        string passwordM = password.text;
+        Debug.Log("Username: " + usernameM);
+        Debug.Log("Password: " + passwordM);
+        
+
+        // Here you can add your code to save the username and password to a file or database
+    }
+}
