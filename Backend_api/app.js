2bb4341c9d8b4895b39e8ce907e40944f4c11051
Francisco Martinez Gallardo <franmgl@Franciscos-MacBook-Pro.local>
2023-04-20 09:55:03 -0600

    Add Basic User apis

---
+++ Backend_api/app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const mysql = require("mysql2/promise");
+
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Route for creating a new user
+async function connectDB(){
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "zazzacrifice",
+      });
+
+    return connection;  
+}
+
+async function  check_if_user_exists(username)
+{
+    // Create a connection to the MySQL database
+    const connection = await connectDB();
+    // Execute a SELECT query to retrieve all users
+    const [rows] = await connection.query("SELECT * FROM users WHERE username = ?", [username]);
+    // Close the database connection
+    await connection.end();
+
+    if (rows.length > 0)
+        return true;
+    else
+        return false;
+}
+
+// Route to get all users
+app.get("/api/users", async (req, res) => {
+  try {
+    console.log(req.body);
+    // Create a connection to the MySQL database
+    const connection = await connectDB();
+    // Execute a SELECT query to retrieve all users
+    
+
+    const [rows] = await connection.query("SELECT * FROM users");
+
+    // Send the users as a JSON response
+    res.json(rows);
+
+    // Close the database connection
+    await connection.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+// Login to Database
+app.post("/api/login", async (req, res) => {
+    try {
+        console.log(req.body);
+        //Create a connection to the MySQL database
+        const connection = await connectDB();
+        // Execute a SELECT query to retrieve all users
+        const [rows] = await connection.query("SELECT * FROM users WHERE username = ? AND password = ?", [req.body.username, req.body.password]);
+        res.json(rows[0].user_id);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// Route to create a new user
+app.post("/api/new_user", async (req, res) => {
+    try {
+        console.log(req.body);
+        //Create a connection to the MySQL database
+        const connection = await connectDB();
+        //Check if user exists
+        if (await check_if_user_exists(req.body.username))
+        {
+            res.status(401).json({ error: "User already exists" });
+            return;
+        }
+        // Execute a SELECT query to retrieve all users
+        const [rows] = await connection.query("INSERT INTO users (username, password) VALUES (?, ?)", [req.body.username, req.body.password]);
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
