7ffc6d9522b2d67334d4f2bde06af3d5bd36cca2
Domingo Mora <A01783317.tec.mx>
2023-04-27 12:18:52 -0600

    Progress

--- videogames/zazacrife/Assets/Scripts/DataSave/FileDataHandler.cs
+++ videogames/zazacrife/Assets/Scripts/DataSave/FileDataHandler.cs
@@ -9,9 +9,10 @@ public class FileDataHandler
    private string dataDirPath = "";
    private string dataFileName = "";
 
-   public FileDataHandler(string dataDirPath, string dataFileName){
-    this.dataDirPath = dataDirPath;
-    this.dataFileName = dataFileName;
+   public FileDataHandler(string dataDirPath, string dataFileName)
+   {
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
    }
    public GameData Load(){
 
@@ -20,6 +21,16 @@ public class FileDataHandler
         string fullPath = Path.Combine(dataDirPath, dataFileName);
         try{
             Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+
+            string dataToStore = JsonUtility.ToJson(data,true);
+
+            using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
         }
         catch (Exception e)
         {
